82311ab84499aeb352e7a379d226e3dfc532188b
Sivupel <91537239+Sivupel@users.noreply.github.com>
2022-10-11 15:38:21 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+int main() {
+	int n, k;
+	int m, l;
+	t = pow(10, 10);
+	std::cin >> n >> k;
+	for (int i = 0; i < n; i += 1) {
+		std::cin >> m;
+
+		if (k % m == 0) {
+			if (k / m < l) {
+				l = k / m;
+			}
+
+		}
+	}
+	std::cout << t;
+}
