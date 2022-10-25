e41bba9afe111776ddc3617eeee0fb3dc501f9aa
Sivupel <91537239+Sivupel@users.noreply.github.com>
2022-10-25 15:58:11 +0300

    Add files via upload

--- Source1.cpp
+++ Source1.cpp
@@ -1,15 +1,24 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
+using namespace std;
+typedef long long ll;
 
+void testcase() {
+	ll n, m;
+	cin >> n >> m;
 
+	for (ll i = 1;i <= n;i++) {
+		for (ll j = 1;j <= m;j++) {
+			cout << ((i % 4 <= 1) != (j % 4 <= 1)) << " \n"[j == m];
+		}
+	}
+}
 int main()
 {
-	int x(0);
-	std::cin >> x;
-	int key = 0;
-	while (n > 0) {
-		key += n & 1;
-		n >>= 1;
-	}
-	std::cout << key << std::endl;
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+		testcase();
+	return 0;
 }
