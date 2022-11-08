ee2030ccab2bf16821d5c502fb8b35a20088bd24
Sivupel <91537239+Sivupel@users.noreply.github.com>
2022-11-08 15:27:38 +0300

    Add files via upload

--- Source1.cpp
+++ Source1.cpp
@@ -1,24 +1,49 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-typedef long long ll;
-
-void testcase() {
-	ll n, m;
-	cin >> n >> m;
-
-	for (ll i = 1;i <= n;i++) {
-		for (ll j = 1;j <= m;j++) {
-			cout << ((i % 4 <= 1) != (j % 4 <= 1)) << " \n"[j == m];
+#include <iostream>
+#include <map>
+int main() {
+	int a, n;
+	std::map<int, int> was;
+	std::cin >> n >> a;
+	was[a] = 0;
+	int earliest = a;
+	for (int i = 1; i < n; i++) {
+		std::cin >> a;
+		was[a] = i;
+	}
+				for (int k = j + 1; k < 6; k++) {
+					if (a[i] + a[j] + a[k] == sum - a[i] - a[j] - a[k]) {
+						cout "YES" << a;
+						return 0;
+					}
+				}
+			}
+		}
+		cout << "NO";
+		return 0;
+	}
+	for (auto i : was) {
+		if (i.second < was[earliest]) {
+			earliest = i.first;
 		}
 	}
+	std::cout << earliest;
 }
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(0);
-	int t;
-	cin >> t;
-	while (t--)
-		testcase();
-	return 0;
+for (i == 0; i >> n;i ++){
+	std::vector<int> b(n);
+	for (int i = 0; i < n; ++i) {
+		std::cin >> a[i];
+	}
+	for (int i = 0; i < n; ++i) {
+		std::cin >> b(n);
+	}
+	std::sort(std::begin(a), std::end(a));
+	int k = 1;
+	int ans = 0;
+	for (int i = 0; i < n; ++i) {
+		if (a[i] >= k) {
+			ans += 1;
+			k += 1;
+		}
+	}
+	std::cout << ans;
 }
